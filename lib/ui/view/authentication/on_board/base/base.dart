bdc5396f8c79decd286ca0fd50d11fce2269bc1f
Yunus Karaaslan <karaaslan.21@hotmail.com>
2021-03-04 18:33:16 +0300

    fixed pages

---
+++ lib/ui/view/authentication/on_board/base/base.dart
@@ -0,0 +1,38 @@
+import 'package:diet_app/core/constant/sizes.dart';
+import 'package:diet_app/core/constant/styles.dart';
+import 'package:flutter/material.dart';
+
+class OnBoardBase extends StatelessWidget {
+  final String asset_image, title, subtitle;
+
+  const OnBoardBase(
+    this.asset_image,
+    this.title,
+    this.subtitle
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 50),
+              child: Image(
+                image: AssetImage(asset_image),
+                height: Sizes.height_30percent(context),
+              ),
+            ),
+          ),
+          Text(title, style: kTitleStyle, textAlign: TextAlign.center,),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: Text(subtitle, style: kSubtitleStyle, textAlign: TextAlign.center,),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/ui/view/authentication/on_board/components/diet_page.dart
+++ lib/ui/view/authentication/on_board/components/diet_page.dart
@@ -1,31 +1,10 @@
-import 'package:diet_app/core/constant/sizes.dart';
-import 'package:diet_app/core/constant/styles.dart';
+import 'package:diet_app/ui/view/authentication/on_board/base/base.dart';
 import 'package:flutter/material.dart';
 import 'package:get/utils.dart';
 
 class DietPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 50),
-              child: Image(
-                image: AssetImage('assets/images/onboard/onboarding3.png'),
-                height: Sizes.height_30percent(context),
-              ),
-            ),
-          ),
-          Text('diet_lists'.tr, style: kTitleStyle, textAlign: TextAlign.center,),
-          Padding(
-            padding: const EdgeInsets.all(15.0),
-            child: Text('onboard3_subtitle'.tr, style: kSubtitleStyle, textAlign: TextAlign.center,),
-          ),
-        ],
-      ),
-    );
+    return OnBoardBase('assets/images/onboard/onboarding3.png','diet_lists'.tr, 'onboard3_subtitle'.tr);
   }
 }
