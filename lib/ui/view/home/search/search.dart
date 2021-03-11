08821461c4332e43d04ad74ad73a7a8a7bf409b8
Yunus Karaaslan <karaaslan.21@hotmail.com>
2021-03-11 16:30:07 +0300

    enhanced code and added search page ui

--- lib/ui/view/home/search/search.dart
+++ lib/ui/view/home/search/search.dart
@@ -1,8 +1,11 @@
 import 'package:diet_app/core/base/view/base_view.dart';
+import 'package:diet_app/core/constant/colors.dart';
+import 'package:diet_app/ui/view/home/search/components/exercises.dart';
 import 'package:diet_app/ui/viewmodel/home/search/search.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import 'components/dietitians.dart';
 import 'components/meals.dart';
 
 class Search extends StatefulWidget {
@@ -12,6 +15,7 @@ class Search extends StatefulWidget {
 
 class _SearchState extends State<Search> {
   final viewModel = Get.put(SearchViewModel());
+
   @override
   Widget build(BuildContext context) {
     return BaseView(
@@ -21,11 +25,20 @@ class _SearchState extends State<Search> {
   }
 
   Widget body(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(10),
+    return SingleChildScrollView(
       child: Column(
         children: [
           Meals(),
+          Container(
+            height: 15,
+            color: AppColors.backgroundColor,
+          ),
+          Dietitians(),
+          Container(
+            height: 15,
+            color: AppColors.backgroundColor,
+          ),
+          Exercises(),
         ],
       ),
     );
